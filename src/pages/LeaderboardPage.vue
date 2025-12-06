d92974bf52623b43a676b4201278b56e932d50f0
Marco Antonio Humberto Decormis Pinto <marco.decormis@friktek.com>
2025-12-06 12:52:39 -0400

    fix: Category filters its done

--- src/pages/LeaderboardPage.vue
+++ src/pages/LeaderboardPage.vue
@@ -13,9 +13,10 @@ const liveRanking = ref([])
 
 const selectedStartGroup = ref(null)
 const categories = computed(() => {
+	const selectedStartGroupName = selectedStartGroup.value?.name
 	// De selectedStartGroup.categories solo quiero que me devuelva un array de name
 	const result = selectedStartGroup.value?.categories.map(
-		(category) => category.name
+		(category) => selectedStartGroupName + ' ' + category.name
 	)
 	console.log('Categories:', result)
 	return result
@@ -40,15 +41,13 @@ const initFilters = () => {
 	filters.value = {
 		global: { value: null, matchMode: FilterMatchMode.CONTAINS },
 		full_name: { value: null, matchMode: FilterMatchMode.CONTAINS },
-		start_group_name: { value: null, matchMode: FilterMatchMode.CONTAINS },
-		category_name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+		full_category: { value: null, matchMode: FilterMatchMode.CONTAINS },
 	}
 }
 const filters = ref({
 	global: { value: null, matchMode: FilterMatchMode.CONTAINS },
 	full_name: { value: null, matchMode: FilterMatchMode.CONTAINS },
-	start_group_name: { value: null, matchMode: FilterMatchMode.CONTAINS },
-	category_name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+	full_category: { value: null, matchMode: FilterMatchMode.CONTAINS },
 })
 
 const clearFilter = () => {
@@ -89,56 +88,32 @@ onMounted(() => {
 					<div class="flex items-center gap-2 text-primary mb-4">
 						<span class="relative flex h-2 w-2">
 							<span
-								class="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"
-							></span>
-							<span
-								class="relative inline-flex rounded-full h-2 w-2 bg-primary"
-							></span>
+								class="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
+							<span class="relative inline-flex rounded-full h-2 w-2 bg-primary"></span>
 						</span>
 						<p class="text-sm font-bold">EN VIVO</p>
 					</div>
 				</template>
 				<template #content>
-					<DataTable
-						v-model:filters="filters"
-						:value="liveRanking"
-						dataKey="id"
-						filterDisplay="menu"
-						class="text-sm"
-						:globalFilterFields="[
+					<DataTable v-model:filters="filters" :value="liveRanking" dataKey="id" filterDisplay="menu"
+						class="text-sm" :globalFilterFields="[
 							'full_name',
 							'start_group_name',
 							'category_name',
-						]"
-					>
+						]">
 						<template #header>
 							<div class="flex flex-col md:flex-row gap-2 mb-4">
 								<IconField>
 									<InputIcon class="pi pi-search" />
-									<InputText
-										v-model="filters['global'].value"
-										placeholder="Buscar por palabra clave"
-										fluid
-									/>
+									<InputText v-model="filters['global'].value" placeholder="Buscar por palabra clave"
+										fluid />
 								</IconField>
-								<Select
-									v-model="selectedStartGroup"
-									:options="event.start_groups"
-									optionLabel="name"
-									placeholder="Seleccionar grupo"
-								/>
-								<Select
-									v-model="filters['category_name'].value"
-									:options="categories"
-									placeholder="Seleccionar categoría"
-								/>
-								<Button
-									type="button"
-									icon="pi pi-filter-slash"
-									label="Limpiar"
-									variant="outlined"
-									@click="clearFilter()"
-								/>
+								<Select v-model="selectedStartGroup" :options="event.start_groups" optionLabel="name"
+									placeholder="Seleccionar grupo" />
+								<Select v-model="filters['full_category'].value" :options="categories"
+									placeholder="Seleccionar categoría" />
+								<Button type="button" icon="pi pi-filter-slash" label="Limpiar" variant="outlined"
+									@click="clearFilter()" />
 							</div>
 						</template>
 						<template #empty> Aun no hay resultados. </template>
@@ -147,21 +122,13 @@ onMounted(() => {
 								{{ data.race_number }}
 							</template>
 						</Column>
-						<Column
-							field="full_name"
-							header="Nombre completo"
-							style="min-width: 14rem"
-						>
+						<Column field="full_name" header="Nombre completo" style="min-width: 14rem">
 							<template #body="{ data }">
 								{{ data.full_name }}
 							</template>
 							<template #filter="{ filterModel, filterCallback }">
-								<InputText
-									v-model="filterModel.value"
-									type="text"
-									@input="filterCallback()"
-									placeholder="Search by full name"
-								/>
+								<InputText v-model="filterModel.value" type="text" @input="filterCallback()"
+									placeholder="Search by full name" />
 							</template>
 						</Column>
 						<Column field="team" header="Equipo" style="min-width: 10rem">
@@ -169,20 +136,15 @@ onMounted(() => {
 								{{ data.team }}
 							</template>
 						</Column>
-						<Column field="category_name" header="Categoría">
+						<Column field="full_category" header="Categoría">
 							<template #body="{ data }">
 								<div class="flex flex-col xl:flex-row xl:gap-1">
-									<span>{{ data.start_group_name }}</span>
-									<span>{{ data.category_name }}</span>
+									<span>{{ data.full_category }}</span>
 								</div>
 							</template>
 							<template #filter="{ filterModel, filterCallback }">
-								<InputText
-									v-model="filterModel.value"
-									type="text"
-									@input="filterCallback()"
-									placeholder="Search by category name"
-								/>
+								<InputText v-model="filterModel.value" type="text" @input="filterCallback()"
+									placeholder="Search by category name" />
 							</template>
 						</Column>
 						<Column field="start_time" header="Inicio">
@@ -215,49 +177,41 @@ onMounted(() => {
 						<Column field="position_in_category" header="Categoría">
 							<template #body="{ data }">
 								{{ data.position_in_category }}
-								<i
-									:class="`${
-										data.position_in_category === 1 ||
-										data.position_in_category === 2 ||
-										data.position_in_category === 3
-											? 'pi pi-trophy'
-											: ''
+								<i :class="`${data.position_in_category === 1 ||
+									data.position_in_category === 2 ||
+									data.position_in_category === 3
+									? 'pi pi-trophy'
+									: ''
 									}
 						
-						${
-							data.position_in_category === 1
-								? ' text-yellow-500 font-bold'
-								: data.position_in_category === 2
-								? ' text-gray-500'
-								: data.position_in_category === 3
-								? ' text-amber-800'
-								: ''
-						}`"
-								></i>
+						${data.position_in_category === 1
+										? ' text-yellow-500 font-bold'
+										: data.position_in_category === 2
+											? ' text-gray-500'
+											: data.position_in_category === 3
+												? ' text-amber-800'
+												: ''
+									}`"></i>
 							</template>
 						</Column>
 						<Column field="position_overall" header="General">
 							<template #body="{ data }">
 								{{ data.position_overall }}
-								<i
-									:class="`${
-										data.position_overall === 1 ||
-										data.position_overall === 2 ||
-										data.position_overall === 3
-											? 'pi pi-crown'
-											: ''
+								<i :class="`${data.position_overall === 1 ||
+									data.position_overall === 2 ||
+									data.position_overall === 3
+									? 'pi pi-crown'
+									: ''
 									}
 						
-						${
-							data.position_overall === 1
-								? ' text-yellow-500 font-bold'
-								: data.position_overall === 2
-								? ' text-gray-500'
-								: data.position_overall === 3
-								? ' text-amber-800'
-								: ''
-						}`"
-								></i>
+						${data.position_overall === 1
+										? ' text-yellow-500 font-bold'
+										: data.position_overall === 2
+											? ' text-gray-500'
+											: data.position_overall === 3
+												? ' text-amber-800'
+												: ''
+									}`"></i>
 							</template>
 						</Column>
 					</DataTable>
